a25f793473c6aa404af5cec4ddb9e120438a82fb
Tom Vajtay <vajtayt@gmail.com>
2016-07-18 15:15:32 -0400

    Add files via upload

---
+++ airsweep_2board-1.ino
@@ -0,0 +1,125 @@
+//Based off of the Arduino servo sweep example
+//Code is to move servo arm with a possible air blower attached in front of a mouse.
+//Code can both repeat a consistent 2 second sweep, or a random sweep from 1-3 secs.
+//By Tom Vajtay 05-19-2016
+
+#include <Wire.h>
+
+char control_char;  //Declares variable
+bool trial_start = false;
+bool randomizer = false;
+long Time = 2000;
+int End;
+int count = 0;
+
+int CamPin = 13;
+
+void setup()
+{
+  
+  pinMode(CamPin, OUTPUT);
+  digitalWrite(CamPin, LOW);
+
+
+  //Uses analog signal from empty A3 to serve as random starting point for random number generation
+  randomSeed(analogRead(3));
+
+  Wire.begin();
+  Serial.begin(9600);
+
+
+  //Serial Monitor Setup
+  Serial.println("Behavioral Setup");
+  Serial.println("Press 1 to start experiment");
+  Serial.println("Press 2 to end session");
+  Serial.println("Press 3 to toggle random servo movement");
+  Serial.println("Press 4 to test servo");
+  Serial.println("Press 5 to toggle LED");
+  delay(500);
+
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    control_char = Serial.read();
+
+    if (control_char == '3') {
+      randomizer = !randomizer;
+      Serial.print("Randomizer:\t");
+      Serial.print("\t");
+      Serial.println(randomizer);
+    }
+
+
+    if (control_char == '4') {
+      Wire.beginTransmission(10);
+      Wire.write(4);
+      Wire.endTransmission();
+    }
+
+    if (control_char == '1') {      //Starts the trial
+      count = 0;
+      Serial.println("Trial Begins");
+      Serial.println();
+      Serial.println("Count#\tDuration");
+      trial_start = true;
+    }
+  }
+  while (trial_start) {
+    if (Serial.available()) {
+      control_char = Serial.read();
+
+      if (control_char == '2') {
+        Serial.println("Session end");
+        //Stops the trial
+        end_session();
+      }
+    }
+    if (randomizer) {
+      randTime();
+      }
+    Serial.print(count);
+    Serial.print("\t");
+    Serial.println(Time);
+
+    Wire.beginTransmission(10);
+    Wire.write(6);
+    Wire.endTransmission();
+   
+    delay(Time - 1000); 
+
+    digitalWrite(CamPin, HIGH);   //Sends TTL pulse to trigger Master 9
+    delay(100);
+    digitalWrite(CamPin, LOW);
+    delay(900);                  //One second delay before air sweep
+
+    Wire.beginTransmission(10);
+    Wire.write(7);
+    Wire.endTransmission();
+
+    delay(8000);
+    
+    count = count + 1;
+  }
+}
+
+
+
+
+
+void randTime() {
+  Time = random(1, 3);
+  Time = Time * 1000;
+
+
+}
+
+void end_session() {
+  Serial.println();
+  Wire.beginTransmission(10);
+  Wire.write(8);
+  Wire.endTransmission();
+  delay(500);
+  asm volatile ("  jmp 0");   // Makes the Arduino soft reset
+}
---
+++ airsweep_2board-2.ino
@@ -0,0 +1,99 @@
+//Based off of the Arduino servo sweep example
+//Code is to move servo arm with a possible air blower attached in front of a mouse.
+//Code can both repeat a consistent 2 second sweep, or a random sweep from 1-3 secs.
+//By Tom Vajtay 05-19-2016
+
+#include <Wire.h>
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int pos;        //Defines initial position
+int step_time = 2;  //Time in ms of each degree of arm movement
+int x = 0;
+void setup()
+{
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  pinMode(9, OUTPUT);
+  
+  myservo.write(2);    //Automatically places servo arm at zero*(Servo can not go perfectly to 0 degrees)
+
+  Wire.begin(10);
+  Wire.onReceive(what2do);
+}
+
+void loop() {
+delay(100);
+
+if (x == 4){
+  servo_test();
+}
+else if(x == 6){
+  servo_event1();
+}
+else if(x == 7){
+  servo_event2();
+}
+
+}
+
+
+
+void servo_test() {
+  for (pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 90 degrees
+  { // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(step_time);                       // waits 180ms for the servo to reach the position
+  }
+
+  delay(500);
+
+  for (pos = 90; pos >= 0; pos -= 1) // goes from 90 degrees to 0 degrees
+  {
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(step_time);                 
+  }
+  x = 0;
+}
+
+void servo_event1(){
+  for (pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 90 degrees
+      { // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(step_time);                       // waits 2ms for the servo to reach the position
+      }
+      x = 0;
+}
+
+void servo_event2(){
+  for (pos = 90; pos >= 0; pos -= 1) // goes from 90 degrees to 0 degrees
+      {
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(step_time);                       // waits 2ms for the servo to reach the position
+      }
+      x = 0;
+}
+
+void what2do(int byteCount){
+
+    int control_char = Wire.read();
+
+    if (control_char == 4) {
+      x = 4;
+    }
+
+    if (control_char == 6){
+      x = 6;
+    }
+
+    if (control_char == 7){
+      x = 7;
+    }
+
+    if (control_char == 8){
+      myservo.write(2);
+    }
+
+
+}
+
